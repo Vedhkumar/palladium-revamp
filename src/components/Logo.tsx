af7318d6b3f9300d6d323a9c0c0edc3fbe2ed61e
Vedh Kumar <vedhkumar237@gmail.com>
2024-12-23 00:43:53 +0530

    logo moodified

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,9 @@
+import logo from "../assets/palladium-logo.webp";
 function Logo() {
   return (
-    <h3 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-orange-500 to-yellow-600  cursor-pointer">
-      Palldium
-    </h3>
+    <h2>
+      <img className="h-10" src={logo} alt="Palladium" />
+    </h2>
   );
 }
 
